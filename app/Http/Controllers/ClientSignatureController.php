c8f40d3fec44c888a17ab1d757b4fa8ff50c811e
simofri97 <simofritet97@gmail.com>
2025-10-15 17:51:28 +0100

    180 y sim

--- app/Http/Controllers/ClientSignatureController.php
+++ app/Http/Controllers/ClientSignatureController.php
@@ -47,7 +47,7 @@ class ClientSignatureController extends Controller
                 'type'        => 'signature',
                 'page'        => 2,
                 'x'           => 120,
-                'y'           => 120,
+                'y'           => 180,
                 'width'       => 180,
                 'height'      => 45,
             ];
