430939fbee7feda6aa3014628cd67894c29f324d
simofri97 <simofritet97@gmail.com>
2025-10-17 00:42:14 +0100

    page1you

--- app/Http/Controllers/ClientSignatureController.php
+++ app/Http/Controllers/ClientSignatureController.php
@@ -45,10 +45,10 @@ class ClientSignatureController extends Controller
             $signatureField = [
                 'type'        => 'signature',
                 'document_id' => $doc['id'],
-                'page'        => 2,
+                'page'        => 1,
                 'shape'       => [
                     'x'      => 120,
-                    'y'      => 760,   // higher on the page
+                    'y'      => 139,   // higher on the page
                     'width'  => 180,
                     'height' => 45,
                 ],
