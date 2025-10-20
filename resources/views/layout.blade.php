be011743c3499cb118aceee7400d497b4df81d07
manal-elmarfoussi10 <manalelmarfoussi@gmail.com>
2025-10-20 18:13:19 +0100

    check now

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>GG AUTO</title>
+    <title>GS AUTO</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Font Awesome & Tailwind -->
